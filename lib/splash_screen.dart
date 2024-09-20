295f80f8198e2f167d1b2895832437b3d3520449
Shree Bhagwat <shree@codeaamy.com>
2024-09-20 16:28:37 +0530

    updated as per the xcode 16

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hashtag/home_screen.dart';
-import 'package:hashtag/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
